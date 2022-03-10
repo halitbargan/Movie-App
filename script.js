5324ef2491971751b38dd6ebbd0649387f293685
halit.bargan <hltbrgn@gmail.com>
2022-03-10 23:51:35 +0200

    start

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
 const container = document.querySelector(".container");
-const allSeats = document.querySelectorAll(".container.seat");
-const notOccupiedSeats =  document.querySelector(".container .seat:not(.occupied)");
+const allSeats = document.querySelectorAll(".container .seat");
+const notOccupiedSeats = document.querySelector(".container .seat:not(.occupied)");
 const count = document.getElementById("count");
 const film = document.getElementById("film");
 const total = document.getElementById("total");
 const movieSelectBox = document.getElementById("movie");
 
-// önce localstorage sonta selectbox
-// initial value == movieSelectBox.value 
-currentTicketPrice = localStorage.getItem("selectedMoviePrice") ? localStorage.getItem("selectedMoviePrice") : movieSelectBox.options[
-    movieSelectBox.selectedIndex].value;
-let currentTicketPrice2 = 10
+//önce localStorage sonra slectBox
+//initial value == movieSelectBox.value
+//movieSelectBox.options[movieSelectBox.selectedIndex].value == movieSelectBox.value
+//(en güncel movie seat price)
+let currentTicketPrice = localStorage.getItem("selectedMoviePrice") ? localStorage.getItem("selectedMoviePrice") : movieSelectBox.options[movieSelectBox.selectedIndex].value;
 
-//movieIndex
-let currentMovieIndex = localStorage.getItem("selectedMovieIndex") ? localStorage.getItem("selectedMoviePrice") : movieSelectBox.selectedIndex;
+//movieIndex (en güncel movie index)
+let currentMovieIndex = localStorage.getItem("selectedMovieIndex") ? localStorage.getItem("selectedMovieIndex") : movieSelectBox.selectedIndex;
 
-    const container = document.querySelector(".container");
-    const allSeats = document.querySelectorAll(".container .seat");
-    const notOccupiedSeats = document.querySelector(".container .seat:not(.occupied)");
-    const count = document.getElementById("count");
-    const film = document.getElementById("film");
-    const total = document.getElementById("total");
-    const movieSelectBox = document.getElementById("movie");
-    
-    movieSelectBox.addEventListener("change", (e)=>{
-        let ticketPrice = e.target.value;
-        let movieIndex = e.target.selectedIndex;
-        console.log(movieIndex);
-        setMovieDataToLocalStorage(ticketPrice, movieIndex);
-    });
-    
-    const setMovieDataToLocalStorage = (ticketPrice, movieIndex) => {
-        localStorage.setItem("selectedMovieIndex", movieIndex);
-        localStorage.setItem("selectedMoviePrice", ticketPrice);
-    }
-    
-    container.addEventListener("click", (e)=>{
+movieSelectBox.addEventListener("change", (e)=>{
+    let ticketPrice = e.target.value;
+    let movieIndex = e.target.selectedIndex;
+    console.log(movieIndex);
+    setMovieDataToLocalStorage(ticketPrice, movieIndex);
+});
+
+const setMovieDataToLocalStorage = (ticketPrice, movieIndex) => {
+    localStorage.setItem("selectedMovieIndex", movieIndex);
+    localStorage.setItem("selectedMoviePrice", ticketPrice);
+}
+
+container.addEventListener("click", (e)=>{
+    console.log(e.target.classList);
+    if(e.target.classList.contains("seat") && !e.target.classList.contains("occupied")){
+        e.target.classList.toggle("selected");
         console.log(e.target.classList);
-        if(e.target.classList.contains("seat") && !e.target.classList.contains("occupied")){
-            e.target.classList.toggle("selected");
-            console.log(e.target.classList);
-        }
-        // if(e.target.classList.contains("seat") && e.target.classList.contains("occupied")){
-        //     alert("lütfen rezerve olmayan koltuk seçiniz!");
-        // }
-        
-    })
-    updateMovieInfo(localStorage.getItem("selectedMoviePrice"));
-})
+    }
+    // if(e.target.classList.contains("seat") && e.target.classList.contains("occupied")){
+    //     alert("lütfen rezerve olmayan koltuk seçiniz!");
+    // }
+    updateMovieInfo();
+});
+
+const updateMovieInfo = () =>{
+    let selectedSeats = document.querySelectorAll(".row .seat.selected");
+    // let selectedSeats2 = document.querySelectorAll(".row .seat .selected");
+
+    let selectedSeatsIndexArray = [...selectedSeats].map(seat => [...allSeats].indexOf(seat));
+    console.log(selectedSeatsIndexArray);
+    localStorage.setItem("selectedSeats", JSON.stringify(selectedSeatsIndexArray));
+    
+}
